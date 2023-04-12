372b63c2002782b1dfc51dd903fe22e6f2886015
'yunghyunchoi' <'asl321@naver.com'>
2023-04-12 14:51:20 +0900

    20230412 최영현

--- src/main/java/com/my/tour/service/ReviewService.java
+++ src/main/java/com/my/tour/service/ReviewService.java
@@ -2,16 +2,21 @@ package com.my.tour.service;
 
 import java.util.List;
 
+import com.my.tour.domain.Reservation;
 import com.my.tour.domain.Review;
 import com.my.tour.domain.ReviewDto;
 import com.my.tour.domain.ReviewImage;
+import com.my.tour.domain.Tour;
 
 public interface ReviewService {
 	List<Review> getReviews();
 	List<ReviewDto> getMyReviews(String userId);
 	List<ReviewImage> getReviewImage(int reviewNum);
-	String getTourName(int tourNum);
-	int addReview(String reviewTitle, String reviewContent, int score);
-	int fixReview(String reviewTitle, String reviewContent, int score);
+	List<Reservation> getReservations(String userId);
+	List<Tour> getTour(int tourNum);
+	int addReview(String reviewTitle, String reviewContent, double score);
+	int addReviewImage(String reviewImageName, int reviewNum);
+	int delReviewImage(int reviewNum);
+	int fixReview(String reviewTitle, String reviewContent, double score);
 	int delReview(int reviewNum);
 }
